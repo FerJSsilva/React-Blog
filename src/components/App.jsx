ab99e23af450d25f8dfefb61837182414f6f9d39
Fernando Silva <fernando.silva@vistajet.com>
2019-05-18 17:50:35 -0300

    #1 - feature: install and configure CSS framework
    To solve this issue it was chosen to use Bulma CSS framework as solution.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
-import logo from '../assets/images/logo.svg';
+import '../assets/styles/style.sass';
+import '../assets/styles/style.css';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h1 className="App-title">Welcome to React</h1>
-            </header>
-            <p className="App-intro">
-              To get started, edit <code>src/App.js</code> and save to reload.
+  render() {
+    return (
+      <div className="App">
+        <body>
+          <h1 class="title">
+            Bulma
+          </h1>
+
+          <p class="subtitle">
+            Modern CSS framework based on <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">Flexbox</a>
+          </p>
+
+          <div class="field">
+            <div class="control">
+              <input class="input" type="text" placeholder="Input" />
+            </div>
+          </div>
+
+          <div class="field">
+            <p class="control">
+              <span class="select">
+                <select>
+                  <option>Select dropdown</option>
+                </select>
+              </span>
             </p>
           </div>
-        );
-    }
-} 
+
+          <div class="buttons">
+            <a class="button is-primary">Primary</a>
+            <a class="button is-link">Link</a>
+          </div>
+        </body>
+      </div>
+    );
+  }
+}
 
 export default App;
