d065d6c30fafaa41e453d10d953e4d85c69c09fa
Arieh Fabbro <arieh.fabbro@vistajet.com>
2019-06-13 22:14:07 -0300

    #4 - feature: Fixed footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import '../assets/styles/debug.css';
+// import '../assets/styles/debug.css';
 import '../assets/styles/style.sass';
 import '../assets/styles/style.css';
+import Footer from './Footer/Footer'
+import Content from './Content/Content'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <footer class="footer">
-          <div class="content has-text-centered">
-            <p>
-              <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-              <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-                is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-            </p>
-          </div>
-        </footer>
+      <div>
+        <Content />
+        <Footer />
       </div>
     );
   }
